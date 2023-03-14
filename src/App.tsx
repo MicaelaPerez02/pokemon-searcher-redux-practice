e62f770aaee5539e5c3f647e2d279cd0f2e7e2cd
Micaela Perez <micaelaperez663@gmail.com>
2023-03-14 12:06:42 -0300

    adding styles in the nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import ListPokemon from "./components/ListPokemon";
 import ViewPokemon from "./components/ViewPokemon";
-
 import "./styles.css";
 import FindPokemon from "./components/FindPokemon";
-
 import { Provider } from "react-redux";
 import { store } from "./store/store";
 import RecordPokemon from "./components/RecordPokemon";
+import Pokedex from "./assets/pokedexLogo.png";
+import "./App.css";
 
 export default function App() {
   return (
     <Provider store={store}>
       <div className="App">
-        <h1>Pokédex</h1>
+        <img src={Pokedex} alt="Logo Pokedex" className="logoPokedex"/>
         <div id="bandejaDeEntrada">
           <div
             style={{ display: "flex", flexDirection: "column", flexGrow: 1 }}
